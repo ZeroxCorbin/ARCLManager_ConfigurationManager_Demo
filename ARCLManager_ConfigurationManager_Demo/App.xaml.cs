1c478b0c35983b553edd4318d41f804a61941988
Jack <jack@jackb.me>
2020-09-14 11:44:54 -0400

    Lots of changes.

--- ARCLManager_ConfigurationManager_Demo/App.xaml.cs
+++ ARCLManager_ConfigurationManager_Demo/App.xaml.cs
@@ -5,13 +5,20 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using ApplicationSettingsNS;
 
 namespace ARCLManager_ConfigurationManager_Demo
 {
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
+    /// 
     public partial class App : Application
     {
+        public static ApplicationSettings_Serializer.ApplicationSettings Settings;
+
+        public App() => Settings = ApplicationSettings_Serializer.Load("appsettings.xml");
+
     }
 }
